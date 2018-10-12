a0f0817955eb523f1decef7680f6d3eb88c0739d
Nicolas Cordoba <nicolas.pablo.cordoba@gmail.com>
2018-10-11 22:14:12 -0300

    primer dia de memo dinamica

--- memDinamica/main.c
+++ memDinamica/main.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+int* reasignar(int* pBuffer,int size);
+int deleteArray(int* pBuffer);
 int inicializar(int* pBuffer,int limite,int parametro);
 int imprimir(int* pBuffer,int limite);
+int* newArray(int size);
 int main()
 {
     int* pArray;
-    pArray = malloc(sizeof(int)*10);
-
+    int*auxArray;
+    pArray = newArray(10);
     inicializar(pArray,10,12);
     imprimir(pArray,10);
+    auxArray=reasignar(pArray,20);
+    if(auxArray!=NULL){
+        pArray=auxArray;
+    }
+    deleteArray(pArray);
 
     return 0;
+}
+int* newArray(int size){
+    int* retorno=NULL;
+    if(size>0 ){
+        retorno = malloc(sizeof(int)*size);
+    }
+    return retorno;
 }
 int inicializar(int* pBuffer,int limite,int parametro){
     int i;
@@ -34,4 +49,18 @@ int imprimir(int* pBuffer,int limite){
     }
     return retorno;
 }
-
+int deleteArray(int* pBuffer){
+    int retorno=-1;
+    if(pBuffer!=NULL){
+        retorno=0;
+        free(pBuffer);
+    }
+    return retorno;
+}
+int* reasignar(int* pBuffer,int size){
+    int* retorno=NULL;
+    if(pBuffer!=NULL && size>0){
+        retorno= realloc(pBuffer,sizeof(int)*size);
+    }
+   return retorno;
+}
